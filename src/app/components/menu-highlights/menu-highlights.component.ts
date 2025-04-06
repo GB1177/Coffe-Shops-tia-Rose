a30bf2895fdc3849dd124fcc53855e55e35346da
GB1177 <gdd_11@hotmail.com>
2025-04-06 14:44:55 -0300

    Correções em Imagens

--- src/app/components/menu-highlights/menu-highlights.component.ts
+++ src/app/components/menu-highlights/menu-highlights.component.ts
@@ -4,15 +4,15 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-menu-highlights',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule],
   templateUrl: './menu-highlights.component.html',
   styleUrl: './menu-highlights.component.scss',
 })
 export class MenuHighlightsComponent {
   carouselImages: string[] = [
-    'assets/images/coffe-products (2).jpg',
-    'assets/images/coffe-products (3).jpg',
-    'assets/images/coffe-products (4).jpg',
-    'assets/images/coffe-products (5).jpg',
+    './assets/images/coffe-products-2.jpg',
+    './assets/images/coffe-products-3.jpg',
+    './assets/images/coffe-products-4.jpg',
+    './assets/images/coffe-products-5.jpg',
   ];
 }
--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -11,25 +11,25 @@ export class ProductService {
         name: 'Café Expresso',
         description: 'Café forte e encorpado.',
         price: 'R$ 5,00',
-        image: 'assets/images/express-coffe (2).jpg',
+        image: 'assets/images/express-coffee-2.jpg',
       },
       {
         name: 'Cappuccino',
         description: 'Café com leite e espuma.',
         price: 'R$ 7,00',
-        image: 'assets/images/capuccino-coffe (1).jpg',
+        image: 'assets/images/cappuccino-coffee-1.jpg',
       },
       {
         name: 'Mocaccino',
         description: 'Café com leite e espuma.',
         price: 'R$ 7,00',
-        image: 'assets/images/capuccino-coffe (2).jpg',
+        image: 'assets/images/mappuccino-coffee-2.jpg',
       },
       {
         name: 'Café Tia rosa 500g (Grão)',
         description: 'Pacote com 500g de Café Tia Rosa.',
         price: 'R$ 30,00',
-        image: 'assets/images/coffe-products (1).jpg',
+        image: 'assets/images/coffee-products-1.jpg',
       },
     ];
   }
